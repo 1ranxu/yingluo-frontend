4910c9fda0c44edde310ef02778faccd6f42e2cc
LUOYINGDEHUIHEN <1574925401@qq.com>
2023-09-14 16:53:19 +0800

    前端初始化

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,17 +1,44 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import {Toast} from "vant";
+import Index from "../pages/Index.vue";
+import Team from "../pages/Team.vue";
+
 const onClickLeft = () => alert('左');
 const onClickRight = () => alert('右');
+
+const active = ref("index");
+const onChange = (index :any) => Toast(`标签 ${index}`);
 </script>
 
 <template>
+<!--  导航栏-->
   <van-nav-bar
       title="标题"
-      left-text="返回"
-      right-text="按钮"
       left-arrow
       @click-left="onClickLeft"
       @click-right="onClickRight"
-  ></van-nav-bar>
+  >
+    <template #right>
+      <van-icon name="search" size="18" />
+    </template>
+  </van-nav-bar>
+
+  <div id="content">
+    <template v-if="active === 'index' ">
+      <Index />
+    </template>
+    <template v-if="active === 'team' ">
+      <Team />
+    </template>
+
+  </div>
+<!--  TabBar-->
+  <van-tabbar v-model="active" @change="onChange">
+    <van-tabbar-item icon="home-o" name="index">主页</van-tabbar-item>
+    <van-tabbar-item icon="search" name="team">队伍</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" name="user">用户</van-tabbar-item>
+  </van-tabbar>
 
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {Button, NavBar} from "vant";
+import {Icon, NavBar, Tabbar, TabbarItem, Toast} from "vant";
+import 'vant/es/toast/style';
 
 const app=createApp(App)
 
-app.use(Button);
 app.use(NavBar);
+app.use(Icon);
+app.use(Tabbar);
+app.use(TabbarItem);
+app.use(Toast);
 app.mount('#app')
