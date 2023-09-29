d6eb06ee96b5f8745ab56122e808b20f9578a6a8
LUOYINGDEHUIHEN <1574925401@qq.com>
2023-09-29 16:57:43 +0800

    add 搜索队伍

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -5,23 +5,47 @@ import {onMounted, ref} from "vue";
 import myAxios from "../plugins/myAxios.js";
 
 const router = useRouter()
+
+const teamList = ref([]);
+
+const searchText = ref('')
 const doAddTeam = () => {
   router.push('/team/add')
 }
-const teamList=ref([]);
-onMounted(async ()=>{
-  const res = await myAxios.get(`/team/list`)
-  if (res.data.code==1){
-    teamList.value=res.data.data;
+/**
+ * 搜索函数
+ */
+const listTeam = async (val) => {
+  const res = await myAxios.get(`/team/list`, {
+    params: {
+      searchText:val,
+    }
+  })
+  if (res.data.code == 1) {
+    teamList.value = res.data.data;
   }
+}
+
+onMounted(async ()=>{
+  await listTeam('');
 })
+
+const onSearch = async (val)=>{
+  await listTeam(val);
+}
+
+
 </script>
 
 <template>
   <div id="teamPage">
+    <van-search v-model="searchText" placeholder="搜索队伍" @search="onSearch"/>
+
     <van-button icon="plus" type="success" @click="doAddTeam">创建队伍</van-button>
     <team-card-list :team-list="teamList"/>
+    <van-empty description="暂无符合要求的队伍" v-if="!teamList || teamList.length < 1"/>
   </div>
+
 </template>
 
 <style scoped>
